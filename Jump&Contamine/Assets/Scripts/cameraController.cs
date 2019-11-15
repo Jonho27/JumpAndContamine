177ae4e18d16d8d881c71d036ba55d0178d03f41
Jon Hodei Martínez <al361886@uji.es>
2019-11-15 11:44:24 +0100

    Movimiento

---
+++ Jump&Contamine/Assets/Scripts/cameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    Vector3 desiredPosition;
+    float smoothSpeed = 10f;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        desiredPosition = new Vector3(transform.position.x, player.transform.position.y, transform.position.z) + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
+
---
+++ Jump&Contamine/Assets/Scripts/playerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+
+    private Vector2 position;
+    public float distanciaLados;
+    public float distanciaAltura;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (transform.position.x > -1.8) {
+                position = new Vector2(transform.position.x - distanciaLados * 50 * Time.deltaTime, transform.position.y + distanciaAltura * 50 * Time.deltaTime);
+                transform.position = position;
+            }
+            
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (transform.position.x < 2)
+            {
+                position = new Vector2(transform.position.x + distanciaLados * 50 * Time.deltaTime, transform.position.y + distanciaAltura * 50 * Time.deltaTime);
+                transform.position = position;
+            }
+            
+        }
+
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            position = new Vector2(transform.position.x, transform.position.y + distanciaAltura * 50 * Time.deltaTime);
+            transform.position = position;
+        }
+
+        
+    }
+}

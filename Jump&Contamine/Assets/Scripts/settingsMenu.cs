8033a994408eac5b5c5102056bcb2bbeec7b7607
pauborrell <pauborrellegea@gmail.com>
2019-12-11 18:34:08 +0100

    FRASES

--- Jump&Contamine/Assets/Scripts/settingsMenu.cs
+++ Jump&Contamine/Assets/Scripts/settingsMenu.cs
@@ -9,6 +9,117 @@ public class settingsMenu : MonoBehaviour
 
     public AudioMixer audioMixer;
 
+
+    public Image fr0;
+    public Image fr1;
+    public Image fr2;
+    public Image fr3;
+    public Image fr4;
+    public Image fr5;
+    public Image fr6;
+    public int number;
+    private float timer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0f;
+        number = Random.Range(0,7);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {timer += Time.deltaTime;
+        activar();
+        
+        if (timer >= 5){
+            
+            number = Random.Range(0,7);
+            Debug.Log(number);
+            timer = 0f;
+            activar();}
+        
+    }
+
+    public void activar()
+    {
+
+        switch (number)
+        {
+            case 0:
+                fr0.GetComponent<Image>().enabled = true;
+                fr1.GetComponent<Image>().enabled = false;
+                fr2.GetComponent<Image>().enabled = false;
+                fr3.GetComponent<Image>().enabled = false;
+                fr4.GetComponent<Image>().enabled = false;
+                fr5.GetComponent<Image>().enabled = false;
+                fr6.GetComponent<Image>().enabled = false;
+                break;
+            case 1:
+                fr1.GetComponent<Image>().enabled = true;
+
+                fr0.GetComponent<Image>().enabled = false;
+                fr2.GetComponent<Image>().enabled = false;
+                fr3.GetComponent<Image>().enabled = false;
+                fr4.GetComponent<Image>().enabled = false;
+                fr5.GetComponent<Image>().enabled = false;
+                fr6.GetComponent<Image>().enabled = false;
+                break;
+            case 2:
+                fr2.GetComponent<Image>().enabled = true;                
+                
+                fr1.GetComponent<Image>().enabled = false;
+                fr0.GetComponent<Image>().enabled = false;
+                fr3.GetComponent<Image>().enabled = false;
+                fr4.GetComponent<Image>().enabled = false;
+                fr5.GetComponent<Image>().enabled = false;
+                fr6.GetComponent<Image>().enabled = false;
+                break;
+            case 3:
+                fr3.GetComponent<Image>().enabled = true;
+
+                fr1.GetComponent<Image>().enabled = false;
+                fr2.GetComponent<Image>().enabled = false;
+                fr0.GetComponent<Image>().enabled = false;
+                fr4.GetComponent<Image>().enabled = false;
+                fr5.GetComponent<Image>().enabled = false;
+                fr6.GetComponent<Image>().enabled = false;
+                break;
+            case 4:
+                fr4.GetComponent<Image>().enabled = true;
+                fr1.GetComponent<Image>().enabled = false;
+                fr2.GetComponent<Image>().enabled = false;
+                fr3.GetComponent<Image>().enabled = false;
+                fr0.GetComponent<Image>().enabled = false;
+                fr5.GetComponent<Image>().enabled = false;
+                fr6.GetComponent<Image>().enabled = false;
+                break;
+            case 5:
+                fr5.GetComponent<Image>().enabled = true;
+                
+                fr1.GetComponent<Image>().enabled = false;
+                fr2.GetComponent<Image>().enabled = false;
+                fr3.GetComponent<Image>().enabled = false;
+                fr4.GetComponent<Image>().enabled = false;
+                fr0.GetComponent<Image>().enabled = false;
+                fr6.GetComponent<Image>().enabled = false;
+                break;
+            case 6:
+                fr6.GetComponent<Image>().enabled = true;
+                fr1.GetComponent<Image>().enabled = false;
+                fr2.GetComponent<Image>().enabled = false;
+                fr3.GetComponent<Image>().enabled = false;
+                fr4.GetComponent<Image>().enabled = false;
+                fr5.GetComponent<Image>().enabled = false;
+                fr0.GetComponent<Image>().enabled = false;
+                break;
+
+        }
+
+    }
+
     public void stopMusic()
     {
         audioMixer.SetFloat("music", -80);

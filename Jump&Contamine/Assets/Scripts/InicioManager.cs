19b5837a13aa16b75e826125d8b49c623a89782d
pauborrell <pauborrellegea@gmail.com>
2019-12-19 11:51:36 +0100

    ICONOS y bugs
    Jorge la Chupa

--- Jump&Contamine/Assets/Scripts/InicioManager.cs
+++ Jump&Contamine/Assets/Scripts/InicioManager.cs
@@ -34,7 +34,7 @@ public class InicioManager : MonoBehaviour
 
         if(timer >= 3)
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(1);
         }
 
         //image.color = Color.Lerp(image.color, Color.clear, speed * Time.deltaTime);

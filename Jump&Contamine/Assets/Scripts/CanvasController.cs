fe30b91dd64b4449698ed6c861111be86d09be3b
pauborrellegea <pauborrellegea@gmail.com>
2021-03-12 11:33:20 +0100

    Mejora UI In game

--- Jump&Contamine/Assets/Scripts/CanvasController.cs
+++ Jump&Contamine/Assets/Scripts/CanvasController.cs
@@ -8,6 +8,7 @@ public class CanvasController : MonoBehaviour
     public GameObject contamine4;
     public GameObject contamine8;
     public GameObject recycle;
+    public GameObject ArrowController;
 
     private float timerContamine;
     private float timerContamine4;
@@ -18,6 +19,11 @@ public class CanvasController : MonoBehaviour
     private bool contamine4Active;
     private bool contamine8Active;
     private bool recycleActive;
+    private bool arrowActive;
+
+    private float CorrectZ, min, max;
+    private Vector3 vectorRotate;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +31,11 @@ public class CanvasController : MonoBehaviour
         contamine4.SetActive(false);
         contamine8.SetActive(false);
         recycle.SetActive(false);
+        arrowActive = true;
+
+        min = 90f;
+        max = -90f;
+        CorrectZ = 90f;
 
         timerContamine = 0f;
         timerContamine4 = 0f;
@@ -37,6 +48,19 @@ public class CanvasController : MonoBehaviour
     {
         if (contamineActive)
         {
+            if(arrowActive ){
+                arrowActive = false;
+                CorrectZ -= 20f;
+                if(CorrectZ < max)
+                {
+                    vectorRotate.z = -90f;
+                    ArrowController.transform.rotation = Quaternion.Euler(vectorRotate);
+                }
+                else
+                {
+                    ArrowController.transform.Rotate(0f,0f,-20f);
+                }
+            }
             timerContamine += Time.deltaTime;
             contamine.SetActive(true);
             contamine4.SetActive(false);
@@ -47,11 +71,25 @@ public class CanvasController : MonoBehaviour
                 contamineActive = false;
                 timerContamine = 0;
                 contamine.SetActive(false);
+                arrowActive = true;
             }
         }
 
         if (contamine4Active)
         {
+            if(arrowActive ){
+                arrowActive = false;
+                CorrectZ -= 20f;
+                if(CorrectZ < max)
+                {
+                    vectorRotate.z = -90f;
+                    ArrowController.transform.rotation = Quaternion.Euler(vectorRotate);
+                }
+                else
+                {
+                    ArrowController.transform.Rotate(0f,0f,-20f);
+                }
+            }
             timerContamine4 += Time.deltaTime;
             contamine4.SetActive(true);
             contamine.SetActive(false);
@@ -62,11 +100,25 @@ public class CanvasController : MonoBehaviour
                 contamine4Active = false;
                 timerContamine4 = 0;
                 contamine4.SetActive(false);
+                arrowActive = true;
             }
         }
 
         if (contamine8Active)
         {
+            if(arrowActive ){
+                arrowActive = false;
+                CorrectZ -= 20f;
+                if(CorrectZ < max)
+                {
+                    vectorRotate.z = -90f;
+                    ArrowController.transform.rotation = Quaternion.Euler(vectorRotate);
+                }
+                else
+                {
+                    ArrowController.transform.Rotate(0f,0f,-20f);
+                }
+            }
             timerContamine8 += Time.deltaTime;
             contamine8.SetActive(true);
             contamine4.SetActive(false);
@@ -77,11 +129,25 @@ public class CanvasController : MonoBehaviour
                 contamine8Active = false;
                 timerContamine8 = 0;
                 contamine8.SetActive(false);
+                arrowActive = true;
             }
         }
 
         if (recycleActive)
         {
+            if(arrowActive ){
+                arrowActive = false;
+                CorrectZ += 20f;
+                if(CorrectZ > min)
+                {
+                    vectorRotate.z = 90f;
+                    ArrowController.transform.rotation = Quaternion.Euler(vectorRotate);
+                }
+                else
+                {
+                    ArrowController.transform.Rotate(0f,0f,20f);
+                }
+            }
             timerRecycle += Time.deltaTime;
             recycle.SetActive(true);
             contamine4.SetActive(false);
@@ -92,6 +158,7 @@ public class CanvasController : MonoBehaviour
                 recycleActive = false;
                 timerRecycle = 0;
                 recycle.SetActive(false);
+                arrowActive = true;
             }
         }
     }
